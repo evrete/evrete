bc0c763fe780a92885c02e09aa5f9754132d6a04
0swald <andrey.bichkevskiy@gmail.com>
2021-01-14 10:02:26 -0600

    code cleanup, condition evaluation listeners, performance improvement

--- evrete-core/src/main/java/org/evrete/api/SharedBetaFactStorage.java
+++ evrete-core/src/main/java/org/evrete/api/SharedBetaFactStorage.java
@@ -1,40 +1,12 @@
 package org.evrete.api;
 
-import java.util.Collection;
-import java.util.function.Predicate;
-
 public interface SharedBetaFactStorage extends Memory, KeyReIterables<ValueRow> {
 
     void ensureDeltaCapacity(int insertCount);
 
-    @Deprecated
-    default void insert(Collection<? extends RuntimeFact> collection, Predicate<RuntimeFact> predicate) {
-        throw new UnsupportedOperationException();
-    }
-
     void delete(RuntimeFact fact);
 
     void insert(RuntimeFact fact);
-
-    @Deprecated
-    default void delete(Collection<? extends RuntimeFact> collection, Predicate<RuntimeFact> predicate) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Deprecated
-    default void clearDeletedKeys() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Deprecated
-    default boolean hasDeletedKeys() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Deprecated
-    default boolean isKeyDeleted(ValueRow row) {
-        throw new UnsupportedOperationException();
-    }
 
     void clear();
 }
