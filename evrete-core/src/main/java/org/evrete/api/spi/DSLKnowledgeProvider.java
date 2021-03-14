58245c39cb8bca02c550bce84c3ef43748c97a88
0swald <andrey.bichkevskiy@gmail.com>
2021-03-14 17:39:15 -0500

    Java DSL initial commits

---
+++ evrete-core/src/main/java/org/evrete/api/spi/DSLKnowledgeProvider.java
@@ -0,0 +1,60 @@
+package org.evrete.api.spi;
+
+import org.evrete.KnowledgeService;
+import org.evrete.api.Knowledge;
+import org.evrete.api.RuntimeContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+
+public interface DSLKnowledgeProvider {
+
+    String getName();
+
+    /**
+     * <p>
+     * Given the sources' URLs, the DSL implementation must return new Knowledge instance.
+     * Depending on the DSL implementation, URLs may refer to plain text resources, Java classes/archives,
+     * JDBC connection strings, files, etc.
+     * </p>
+     *
+     * @param resources remote or local resources to apply
+     * @param service   Knowledge service.
+     */
+    Knowledge create(KnowledgeService service, URL... resources) throws IOException;
+
+    /**
+     * <p>
+     * Given the sources' URLs, the DSL implementation must compile and append
+     * rules to the provided context.
+     * </p>
+     *
+     * @param targetContext the runtime to append rules to
+     * @param resources     remote or local resources to apply
+     */
+    void apply(RuntimeContext<?> targetContext, URL... resources) throws IOException;
+
+    /**
+     * <p>
+     * Given the input stream, the DSL implementation must compile and append
+     * rules to the provided context.
+     * </p>
+     *
+     * @param targetContext the runtime to append rules to
+     * @param inputStream   input stream to a ruleset data
+     */
+    default void apply(RuntimeContext<?> targetContext, InputStream inputStream) throws IOException {
+        throw new UnsupportedOperationException("Method not supported by " + getClass().getName());
+    }
+
+    /**
+     * @param targetContext the runtime to append rules to
+     * @param reader        reader of a ruleset data
+     * @see #apply(RuntimeContext, InputStream)
+     */
+    default void apply(RuntimeContext<?> targetContext, Reader reader) throws IOException {
+        throw new UnsupportedOperationException("Method not supported by " + getClass().getName());
+    }
+}
