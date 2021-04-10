9f07d6743a96ced688b2257e7c1dc11d1f0be41c
0swald <andrey.bichkevskiy@gmail.com>
2021-04-10 01:10:05 -0500

    performance bug fixes

--- evrete-core/src/main/java/org/evrete/runtime/KeyMemoryBucket.java
+++ evrete-core/src/main/java/org/evrete/runtime/KeyMemoryBucket.java
@@ -17,6 +17,7 @@ abstract class KeyMemoryBucket extends MemoryComponent {
             return false;
         }
     };
+
     final KeyedFactStorage fieldData;
     final ActiveField[] activeFields;
     final Collection<FactHandleVersioned> insertData = new LinkedList<>();
@@ -32,20 +33,20 @@ abstract class KeyMemoryBucket extends MemoryComponent {
         if (alphaMask.isEmpty()) {
             switch (typeFields.size()) {
                 case 0:
-                    return new KeyMemoryBucketNoAlpha0(runtime, typeFields);
+                    return new KeyMemoryBucketNoAlpha.KeyMemoryBucketNoAlpha0(runtime, typeFields);
                 case 1:
-                    return new KeyMemoryBucketNoAlpha1(runtime, typeFields);
+                    return new KeyMemoryBucketNoAlpha.KeyMemoryBucketNoAlpha1(runtime, typeFields);
                 default:
-                    return new KeyMemoryBucketNoAlphaN(runtime, typeFields);
+                    return new KeyMemoryBucketNoAlpha.KeyMemoryBucketNoAlphaN(runtime, typeFields);
             }
         } else {
             switch (typeFields.size()) {
                 case 0:
-                    return new KeyMemoryBucketAlpha0(runtime, typeFields, alphaMask);
+                    return new KeyMemoryBucketAlpha.KeyMemoryBucketAlpha0(runtime, typeFields, alphaMask);
                 case 1:
-                    return new KeyMemoryBucketAlpha1(runtime, typeFields, alphaMask);
+                    return new KeyMemoryBucketAlpha.KeyMemoryBucketAlpha1(runtime, typeFields, alphaMask);
                 default:
-                    return new KeyMemoryBucketAlphaN(runtime, typeFields, alphaMask);
+                    return new KeyMemoryBucketAlpha.KeyMemoryBucketAlphaN(runtime, typeFields, alphaMask);
             }
         }
     }
@@ -75,5 +76,164 @@ abstract class KeyMemoryBucket extends MemoryComponent {
     @Override
     public final String toString() {
         return fieldData.toString();
+    }
+
+    abstract static class KeyMemoryBucketAlpha extends KeyMemoryBucket {
+        private final AlphaBucketMeta alphaMask;
+
+        KeyMemoryBucketAlpha(MemoryComponent runtime, FieldsKey typeFields, AlphaBucketMeta alphaMask) {
+            super(runtime, typeFields);
+            this.alphaMask = alphaMask;
+        }
+
+        @Override
+        final void insert(Iterable<RuntimeFact> facts) {
+            current = DUMMY_FACT;
+            for (RuntimeFact fact : facts) {
+                if (alphaMask.test(fact.alphaTests)) {
+                    if (current.sameValues(fact)) {
+                        insertData.add(fact.factHandle);
+                    } else {
+                        // Key changed, ready for batch insert
+                        flushBuffer();
+                        insertData.add(fact.factHandle);
+                        current = fact;
+                    }
+                }
+            }
+
+            if (!insertData.isEmpty()) {
+                flushBuffer();
+            }
+        }
+
+        static class KeyMemoryBucketAlpha0 extends KeyMemoryBucketAlpha {
+
+            KeyMemoryBucketAlpha0(MemoryComponent runtime, FieldsKey typeFields, AlphaBucketMeta alphaMask) {
+                super(runtime, typeFields, alphaMask);
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
+
+        static class KeyMemoryBucketAlpha1 extends KeyMemoryBucketAlpha {
+            private final ActiveField field;
+
+            KeyMemoryBucketAlpha1(MemoryComponent runtime, FieldsKey typeFields, AlphaBucketMeta alphaMask) {
+                super(runtime, typeFields, alphaMask);
+                assert typeFields.size() == 1;
+                this.field = typeFields.getFields()[0];
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    fieldData.write(currentFactField(field));
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
+
+        static class KeyMemoryBucketAlphaN extends KeyMemoryBucketAlpha {
+
+            KeyMemoryBucketAlphaN(MemoryComponent runtime, FieldsKey typeFields, AlphaBucketMeta alphaMask) {
+                super(runtime, typeFields, alphaMask);
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    for (ActiveField field : activeFields) {
+                        fieldData.write(currentFactField(field));
+                    }
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
+    }
+
+    abstract static class KeyMemoryBucketNoAlpha extends KeyMemoryBucket {
+
+        KeyMemoryBucketNoAlpha(MemoryComponent runtime, FieldsKey typeFields) {
+            super(runtime, typeFields);
+        }
+
+        @Override
+        final void insert(Iterable<RuntimeFact> facts) {
+            current = DUMMY_FACT;
+            for (RuntimeFact fact : facts) {
+                if (current.sameValues(fact)) {
+                    insertData.add(fact.factHandle);
+                } else {
+                    // Key changed, ready for batch insert
+                    flushBuffer();
+                    insertData.add(fact.factHandle);
+                    current = fact;
+                }
+            }
+            if (!insertData.isEmpty()) {
+                flushBuffer();
+            }
+        }
+
+        static class KeyMemoryBucketNoAlpha0 extends KeyMemoryBucketNoAlpha {
+
+            KeyMemoryBucketNoAlpha0(MemoryComponent runtime, FieldsKey typeFields) {
+                super(runtime, typeFields);
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
+
+        static class KeyMemoryBucketNoAlpha1 extends KeyMemoryBucketNoAlpha {
+            private final ActiveField field;
+
+            KeyMemoryBucketNoAlpha1(MemoryComponent runtime, FieldsKey typeFields) {
+                super(runtime, typeFields);
+                assert typeFields.size() == 1;
+                this.field = typeFields.getFields()[0];
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    fieldData.write(currentFactField(field));
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
+
+        static class KeyMemoryBucketNoAlphaN extends KeyMemoryBucketNoAlpha {
+
+            KeyMemoryBucketNoAlphaN(MemoryComponent runtime, FieldsKey typeFields) {
+                super(runtime, typeFields);
+            }
+
+            @Override
+            final void flushBuffer() {
+                if (current != DUMMY_FACT) {
+                    for (ActiveField field : activeFields) {
+                        fieldData.write(currentFactField(field));
+                    }
+                    fieldData.write(insertData);
+                    insertData.clear();
+                }
+            }
+        }
     }
 }
