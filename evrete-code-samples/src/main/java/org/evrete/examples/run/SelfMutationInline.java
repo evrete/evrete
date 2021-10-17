62bf9b014ba146f8b31c961131b83aadbb3d24aa
0swald <andrey.bichkevskiy@gmail.com>
2021-10-16 23:14:01 -0500

    new code examples

--- evrete-code-samples/src/main/java/org/evrete/examples/run/SelfMutationInline.java
+++ evrete-code-samples/src/main/java/org/evrete/examples/run/SelfMutationInline.java
@@ -1,45 +1,52 @@
 package org.evrete.examples.run;
 
 import org.evrete.KnowledgeService;
+import org.evrete.api.Rule;
 import org.evrete.api.StatefulSession;
 
 import static java.lang.System.out;
+
 public class SelfMutationInline {
 
     public static void main(String[] args) {
         KnowledgeService service = new KnowledgeService();
         StatefulSession session = service
-                .newKnowledge()
-                .newRule("Root rule")
-                .forEach(
-                        "$s", StatefulSession.class,
-                        "$e", String.class
-                )
-                .execute(ctx -> {
-                    StatefulSession sess = ctx.get("$s");
-                    String event = ctx.get("$e");
-                    // Appending new rule with the event's name
-                    sess
-                            .newRule(event)
-                            .forEach("$i", Integer.class)
-                            .where("$i % 2 == 0")
-                            .execute(c-> {
-                                out.println(event + ":\t" + c.get("$i"));
-                            });
-                    out.println("New rule created: '" + event + "'");
-                })
-                .newStatefulSession();
+            .newKnowledge()
+            .newRule("Root rule")
+            .forEach(
+            "$s", StatefulSession.class,
+            "$e", String.class
+            )
+            .execute(ctx -> {
+                StatefulSession sess = ctx.get("$s");
+                String event = ctx.get("$e");
+                // Appending new rule with the event's name
+                sess
+                    .newRule(event)
+                    .forEach("$i", Integer.class)
+                    .where("$i % 2 == 0")
+                    .execute(c -> {
+                        out.println(event + ":\t" + c.get("$i"));
+                    });
+                out.println("New rule created: '" + event + "'");
+            })
+            .newStatefulSession();
 
         // 1. Inserting session into self
         session.insertAndFire(session);
         // 2. Nothing happens, the rule requires a String to activate
         session.insertAndFire("Rule1");
         // 3. Testing the new rule
-        session.insertAndFire(0,1,2,3,4,5,6);
+        session.insertAndFire(1, 2, 3, 4, 5, 6);
         // 4. Another event
         session.insertAndFire("Rule2");
         // 5. And another test
-        session.insertAndFire(0,1,2,3,4,5,6);
+        session.insertAndFire(1, 2, 3, 4, 5, 6);
+        // 6. Listing session's rules
+        out.println("\nSession's rules:");
+        for (Rule r : session.getRules()) {
+            out.println(r.getName());
+        }
 
         service.shutdown();
     }
