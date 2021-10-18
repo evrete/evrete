e5519688249d72eed5d55500846c5f2ef49b86b4
0swald <andrey.bichkevskiy@gmail.com>
2021-10-18 16:26:26 -0500

    new code examples

--- evrete-code-samples/src/main/java/org/evrete/examples/howto/ChangingConditions.java
+++ evrete-code-samples/src/main/java/org/evrete/examples/howto/ChangingConditions.java
@@ -2,48 +2,50 @@ package org.evrete.examples.howto;
 
 import org.evrete.KnowledgeService;
 import org.evrete.api.*;
+import static java.lang.System.out;
 
 public class ChangingConditions {
 
     public static void main(String[] args) throws Exception {
         KnowledgeService service = new KnowledgeService();
-
         Knowledge knowledge = service.newKnowledge();
-        RuleBuilder<Knowledge> builder = knowledge
-                .newRule("Even numbers");
 
-
+        // 1. Creating a rule and getting its condition handle
+        RuleBuilder<Knowledge> builder = knowledge.newRule("Even numbers");
         LhsBuilder<Knowledge> lhsBuilder = builder.forEach("$i", Integer.class);
         EvaluatorHandle handle = lhsBuilder.addWhere("$i % 2 == 0");
         lhsBuilder.execute(ctx -> {
-            int evenNumber = ctx.get("$i");
-            System.out.printf("\t%d%n", evenNumber);
+            int $i = ctx.get("$i");
+            out.printf("\t%d%n", $i);
         });
+        out.println("1. Rule created.");
 
-        try(StatefulSession s = knowledge.newStatefulSession()) {
-            // 1. Initial test
-            System.out.println("1. Testing the rule as-is:");
-            s.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
-
+        try (StatefulSession s = knowledge.newStatefulSession()) {
             RuntimeRule rule = s.getRule("Even numbers");
+            // 2. Initial test
+            out.println("2. Testing the rule as-is:");
+            s.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
 
-            Evaluator newCondition =  rule.buildExpression("$i % 2 == 1");
+            // 3. Building a new expression and replacing the old one
+            Evaluator newCondition1 = rule.buildExpression("$i % 2 == 1");
+            s.replaceEvaluator(handle, newCondition1);
+            out.println("3. Testing new condition:");
+            s.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
 
-            s.replaceEvaluator(handle, newCondition);
-            //s.replaceEvaluator();
-
-            rule.setName("Odd numbers");
-
-            System.out.println("2. Testing new condition:");
+            // 4. Yet another condition, as a Predicate this time
+            s.replaceEvaluator(handle, t -> {
+                int $i = t.get(0);
+                return $i % 3 == 0;
+            });
+            out.println("4. Testing another condition:");
             s.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
         }
 
-        try(StatefulSession s = knowledge.newStatefulSession()) {
-            // 1. Initial test
-            System.out.println("3. Testing another session:");
+        try (StatefulSession s = knowledge.newStatefulSession()) {
+            // 5. New session shouldn't be affected by the changes
+            out.println("5. Testing a new session:");
             s.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
         }
-
 
         service.shutdown();
     }
