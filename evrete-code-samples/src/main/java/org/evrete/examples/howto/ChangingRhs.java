73e4acaa0c246f1d948330ba004f25cc3a823d52
0swald <andrey.bichkevskiy@gmail.com>
2021-10-18 13:01:03 -0500

    new code examples

--- evrete-code-samples/src/main/java/org/evrete/examples/howto/ChangingRhs.java
+++ evrete-code-samples/src/main/java/org/evrete/examples/howto/ChangingRhs.java
@@ -2,7 +2,7 @@ package org.evrete.examples.howto;
 
 import org.evrete.KnowledgeService;
 import org.evrete.api.StatefulSession;
-
+import static java.lang.System.out;
 public class ChangingRhs {
 
     public static void main(String[] args) throws Exception {
@@ -15,27 +15,27 @@ public class ChangingRhs {
                 .where("$i % 2 == 0")
                 .execute(ctx -> {
                     int evenNumber = ctx.get("$i");
-                    System.out.printf("\t%d%n", evenNumber);
+                    out.printf("\t%d%n", evenNumber);
                 })
                 .newStatefulSession();
 
         // 1. Initial test
-        System.out.println("1. Testing the rule as-is:");
+        out.println("1. Testing the rule as-is:");
         session.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
 
         // 2. Replacing the RHS
-        System.out.println("2. Replacing the action:");
+        out.println("2. Replacing the action:");
         session.getRule("Even numbers").setRhs(ctx -> {
-            int evenNumber = ctx.get("$i");
-            System.out.println("\t'" + evenNumber + "'");
+            int $i = ctx.get("$i");
+            System.out.println("\t'" + $i + "'");
         });
         session.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
 
         // 3. Chaining actions
         System.out.println("3. Chaining rule's actions:");
         session.getRule("Even numbers").chainRhs(ctx -> {
-            int evenNumber = ctx.get("$i");
-            System.out.println("\tChained action on '" + evenNumber + "'");
+            int $i = ctx.get("$i");
+            out.println("\tChained action on '" + $i + "'");
         });
         session.insertAndFire(0, 1, 2, 3, 4, 5, 6, 7);
 
